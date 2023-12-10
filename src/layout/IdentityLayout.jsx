953bfba980658223cecb6eba3b4f04d0920fabd0
Hamid Asadi <hamid.19471@gmail.com>
2023-12-11 00:29:54 +0330

    Create: main layout for identity features ( Login , Register ) component

---
+++ src/layout/IdentityLayout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import ChangeLanguage from "../components/ui/ChangeLanguage";
+
+const IdentityLayout = () => {
+  return (
+    <div className="md:w-full h-screen flex flex-col items-center justify-between">
+      <header className="p-6 bg-slate-800/30 backdrop-blur-lg shadow-xl flex w-full items-center justify-between">
+        <nav>
+          <ChangeLanguage />
+        </nav>
+        <div>asadi</div>
+      </header>
+      <main className="w-full md:px-12 flex items-center justify-center h-full">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+export default IdentityLayout;
