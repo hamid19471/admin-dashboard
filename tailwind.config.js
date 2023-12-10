295999cdcce35950ab8ad7998360918ff8cfd096
Hamid Asadi <hamid.19471@gmail.com>
2023-12-11 00:18:26 +0330

    install react by vite, install packages, import fonts, locales and ....

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+import DaisyUIPlugin from "daisyui";
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [DaisyUIPlugin],
+};
