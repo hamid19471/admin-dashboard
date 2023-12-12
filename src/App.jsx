f2c4c58e45518430c149dd4277a1295c9f9e301d
Hamid Asadi <hamid.19471@gmail.com>
2023-12-13 00:32:58 +0330

    Update: wrap whole application with Toaster

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import { route } from "./routes";
-
+import { Toaster } from "react-hot-toast";
+import "./core/i18n";
 const App = () => {
-  return <RouterProvider router={route} />;
+  return (
+    <>
+      <RouterProvider router={route} />;
+      <Toaster position="bottom-center" />
+    </>
+  );
 };
 
 export default App;
