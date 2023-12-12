3794b60af2dbd33ea90a7d25c5019a1fd650c14c
Hamid Asadi <hamid.19471@gmail.com>
2023-12-13 00:32:23 +0330

    Update: for main layout and pages

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,14 +3,29 @@ import Login from "./Features/Identity/Components/Login/Login";
 import IdentityLayout from "./layout/IdentityLayout";
 import { RegisterSubmitAction } from "./Features/Identity/Components/Register/RegisterSubmitAction";
 import Register from "./Features/Identity/Components/Register/Register";
+import { LoginAction } from "./Features/Identity/Components/Login/LoginAction";
+import MainLayout from "./layout/MainLayout";
+import Courses from "./pages/Courses";
 
 export const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        element: <Courses />,
+        index: true,
+      },
+    ],
+  },
   {
     element: <IdentityLayout />,
     children: [
       {
         path: "/login",
         element: <Login />,
+        action: LoginAction,
+        errorElement: <Login />,
       },
       {
         path: "/register",
