9769e3f2afeaed2a9674d6a1558e5bd64863b61f
Hamid Asadi <hamid.19471@gmail.com>
2023-12-13 00:34:42 +0330

    Update: use change language context, handle flags, handle direction

--- src/components/ChangeLnaguage.jsx
+++ src/components/ChangeLnaguage.jsx
@@ -1,8 +1,10 @@
 import usFlag from "../assets/images/us.png";
 import faFlag from "../assets/images/fa.png";
 import { useEffect, useRef, useState } from "react";
+import { useAppContext } from "../context/AppContext";
 
 const ChangeLanguage = () => {
+  const { language, changeLanguage } = useAppContext();
   const [show, setShow] = useState(false);
   const ref = useRef();
   useEffect(() => {
@@ -22,7 +24,7 @@ const ChangeLanguage = () => {
     <div>
       <div className="relative" onClick={() => setShow(true)}>
         <img
-          src={usFlag}
+          src={language === "fa" ? faFlag : usFlag}
           alt="English"
           className="rounded-full w-8 h-8 cursor-pointer"
         />
@@ -32,11 +34,17 @@ const ChangeLanguage = () => {
             show ? "lng_show" : "lng_hide"
           }`}
         >
-          <div className="flex items-center justify-star p-2 gap-2 lng_hover cursor-pointer mb-2 text-right">
+          <a
+            className="flex items-center justify-star cursor-pointer p-2 gap-2 lng_hover mb-2 text-right"
+            onClick={() => changeLanguage("en")}
+          >
             <img src={usFlag} alt="English" className="w-7 h-7 rounded-full" />
             <span>English</span>
-          </div>
-          <a className="flex items-center justify-start gap-2 p-2 lng_hover cursor-pointer text-right">
+          </a>
+          <a
+            className="flex items-center justify-start cursor-pointer gap-2 p-2 lng_hover  text-right"
+            onClick={() => changeLanguage("fa")}
+          >
             <img src={faFlag} alt="Farsi" className="w-7 h-7 rounded-full" />
             <span>فارسی</span>
           </a>
