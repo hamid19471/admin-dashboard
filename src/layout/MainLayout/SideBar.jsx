84359fad43e359fe2a1af9fe5736d216a8db9bcc
Hamid Asadi <hamid.19471@gmail.com>
2023-12-16 02:01:53 +0330

    Update: link to all courses and categories page in side bar nav bar

--- src/layout/MainLayout/SideBar.jsx
+++ src/layout/MainLayout/SideBar.jsx
@@ -1,5 +1,7 @@
+import { AcademicCapIcon, TagIcon } from "@heroicons/react/24/solid";
 import { useAppContext } from "../../context/AppContext";
 import logo from "../../assets/images/logo.svg";
+import { Link } from "react-router-dom";
 
 const SideBar = () => {
   const { sideBar } = useAppContext();
@@ -14,8 +16,22 @@ const SideBar = () => {
       <div className="w-full px-8 pt-8">
         <h3 className="font-semibold text-lg text-white">مدیریت دوره‌ها</h3>
         <ul className="flex flex-col gap-3 mt-3 px-4">
-          <li>hamid</li>
-          <li>hamid</li>
+          <li className="flex items-center gap-1">
+            <AcademicCapIcon className="h-4 w-4 text-white" />
+            <Link to="/">
+              <span className="text-sm hover:text-white transition-all duration-300">
+                تمامی دوره ها
+              </span>
+            </Link>
+          </li>
+          <li className="flex items-center gap-1">
+            <TagIcon className="h-4 w-4 text-white" />
+            <Link to="/course-categories">
+              <span className="text-sm hover:text-white transition-all duration-300">
+                دسته بندی دوره ها
+              </span>
+            </Link>
+          </li>
           <li>hamid</li>
           <li>hamid</li>
           <li>hamid</li>
