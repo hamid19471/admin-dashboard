6c3f93e015bff20f73abfc075587dca94f5fac6e
Hamid Asadi <hamid.19471@gmail.com>
2023-12-15 13:53:59 +0330

    Update: create new instance for fetch data with headers

--- src/core/httpService.js
+++ src/core/httpService.js
@@ -5,3 +5,18 @@ const BASE_URL = "https://react-mini-projects-api.classbon.com";
 export const httpService = axios.create({
   baseURL: BASE_URL,
 });
+
+export const httpServiceAuth = axios.create({
+  baseURL: BASE_URL,
+});
+
+httpServiceAuth.interceptors.request.use(async (config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers = {
+      authorization: `Bearer ${token}`,
+    };
+    return config;
+  }
+  (error) => Promise.error(error);
+});
