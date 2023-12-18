c664cf7d5843f0446870bab0f84df7f3fa4d6d0c
Hamid Asadi <hamid.19471@gmail.com>
2023-12-18 12:50:48 +0330

    Fix: issue for params

--- src/actions/SingleCourseLoader.jsx
+++ src/actions/SingleCourseLoader.jsx
@@ -1,12 +1,12 @@
 import { defer } from "react-router-dom";
 import { httpServiceAuth } from "../core/httpService";
-export const SingleCourseLoader = async (params) => {
+export const SingleCourseLoader = async ({ params }) => {
   return defer({
     singleCourse: loadSingleData(params),
   });
 };
 
-const loadSingleData = async ({ params }) => {
+const loadSingleData = async (params) => {
   const { data } = await httpServiceAuth.get(`/Course/by-id/${params.id}`);
   return data;
 };
