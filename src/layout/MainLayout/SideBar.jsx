8141bb80a4ef8bb3837b11c7273d833a2ad0676a
Hamid Asadi <hamid.19471@gmail.com>
2023-12-23 01:59:33 +0330

    Update: sidebar menu

--- src/layout/MainLayout/SideBar.jsx
+++ src/layout/MainLayout/SideBar.jsx
@@ -1,4 +1,9 @@
-import { AcademicCapIcon, TagIcon } from "@heroicons/react/24/solid";
+import {
+  AcademicCapIcon,
+  TagIcon,
+  ReceiptPercentIcon,
+  UserGroupIcon,
+} from "@heroicons/react/24/solid";
 import { useAppContext } from "../../context/AppContext";
 import logo from "../../assets/images/logo.svg";
 import { Link } from "react-router-dom";
@@ -32,19 +37,35 @@ const SideBar = () => {
               </span>
             </Link>
           </li>
-          <li>hamid</li>
-          <li>hamid</li>
-          <li>hamid</li>
+          <li className="flex items-center gap-1">
+            <ReceiptPercentIcon className="h-4 w-4 text-white" />
+            <Link to="/discounts">
+              <span className="text-sm hover:text-white transition-all duration-300">
+                تخفیف دوره ها
+              </span>
+            </Link>
+          </li>
         </ul>
       </div>
       <div className="w-full px-8 pt-8 ">
-        <h3 className="font-semibold text-lg text-white">مدیریت دوره‌ها</h3>
+        <h3 className="font-semibold text-lg text-white">مدیریت کاربران</h3>
         <ul className="flex flex-col gap-3 mt-3 px-4">
-          <li>hamid</li>
-          <li>hamid</li>
-          <li>hamid</li>
-          <li>hamid</li>
-          <li>hamid</li>
+          <li className="flex items-center gap-1">
+            <UserGroupIcon className="h-4 w-4 text-white" />
+            <Link to="/teachers">
+              <span className="text-sm hover:text-white transition-all duration-300">
+                مدیریت مدرسین
+              </span>
+            </Link>
+          </li>
+          <li className="flex items-center gap-1">
+            <UserGroupIcon className="h-4 w-4 text-white" />
+            <Link to="/users">
+              <span className="text-sm hover:text-white transition-all duration-300">
+                مدیریت کاربران
+              </span>
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
